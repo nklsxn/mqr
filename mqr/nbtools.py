c1e91ffc2ba84a9f6e97aac2f67e9170ced76a0e
Nikolas Crossan <nik.crossan@gmail.com>
2024-07-24 23:45:32 +1000

    Add example with dataframe output layouts

--- mqr/nbtools.py
+++ mqr/nbtools.py
@@ -16,3 +16,23 @@ def set_container_width(width_pct: int):
     from IPython.core.display import display, HTML
     assert width_pct > 0 and width_pct <= 100, f'Argument `width_pct={width_pct}` must be 0<width_pct<=100'
     display(HTML(f'<style>.container {{ width:{width_pct}% !important; }}</style>'))
+
+def vstack(*dfs, margin=10):
+    from IPython.core.display import HTML
+    raw_html = '<div style="display:flex; flex-direction:column;">'
+    for df in dfs:
+        raw_html += f'<div style="margin:{margin}px">'
+        raw_html += df._repr_html_()
+        raw_html += '</div>'
+    raw_html += '</div>'
+    return HTML(raw_html)
+
+def hstack(*dfs, margin=10):
+    from IPython.core.display import HTML
+    raw_html = '<div style="display:flex">'
+    for df in dfs:
+        raw_html += f'<div style="margin:{margin}px">'
+        raw_html += df._repr_html_()
+        raw_html += '</div>'
+    raw_html += '</div>'
+    return HTML(raw_html)
