b32cf318f4f0dc40e913dc332719c69fec054af5
Nikolas Crossan <nik.crossan@gmail.com>
2024-08-19 18:07:55 +1000

    Add masked clipping util

--- mqr/utils.py
+++ mqr/utils.py
@@ -20,3 +20,24 @@ def make_ordinal(n):
         number = str(int(n))
         suffix = ['th', 'st', 'nd', 'rd', 'th'][min(int(n) % 10, 4)]
     return number + suffix
+
+def clip_where(a, a_min, a_max, where):
+    '''
+    Clamp `a` so that `a_min <= a <= a_max` is true or all true, masked by `where`.
+
+    Arguments
+    ---------
+    a (number or array) -- value or values to clip.
+    a_min (number) -- minimum value.
+    a_max (number) -- maximum value.
+    where (bool or array[bool]) -- clip when this value or corresponding element
+        evaluates to `True`.
+    '''
+    aa = np.atleast_1d(a).copy()
+    aa[where] = np.clip(np.atleast_1d(a)[where], a_min, a_max)
+    try:
+        iter(a)
+    except TypeError:
+        return aa[0]
+    else:
+        return aa
