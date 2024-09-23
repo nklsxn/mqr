9981d378309fd7e74d241d6db0a6cd1a78acc91b
Nikolas Crossan <nik.crossan@gmail.com>
2024-09-23 16:35:44 +1000

    Add reference

--- mqr/plot/lib/anova.py
+++ mqr/plot/lib/anova.py
@@ -4,10 +4,18 @@ def groups(groups_df, ax):
     """
     Draw a bar graph with error bars for the groups in an ANOVA.
 
+    See [1] (pp. 538-541) for construction of t-statistic.
+
     Arguments
     ---------
     ci (mqr.confint.ConfidenceInterval) -- The confidence interval to draw.
     ax (matplotlib.axes.Axes) -- Axes for the plot.
+
+    References
+    ----------
+    [1] Saville, David J., and Graham R. Wood.
+        Statistical methods: The geometric approach.
+        Springer Science & Business Media, 2012.
     """
     y_err = (groups_df.iloc[:, -1] - groups_df.iloc[:, -2]) / 2
     ax.errorbar(
