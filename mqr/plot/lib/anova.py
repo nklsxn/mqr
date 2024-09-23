037a1eaafcc5dc5f081b4d60f84e99ee02d93925
Nikolas Crossan <nik.crossan@gmail.com>
2024-09-23 16:30:48 +1000

    Improve ANOVA group means plot
    - Plot only the mean (not whole bar) to show relevant info
    - Plot caps on the error bars
    - Extend xlims beyond extreme points half the average distance between ticks
      for better spacing of grouped information

--- mqr/plot/lib/anova.py
+++ mqr/plot/lib/anova.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def groups(groups_df, ax):
     """
     Draw a bar graph with error bars for the groups in an ANOVA.
@@ -8,5 +10,14 @@ def groups(groups_df, ax):
     ax (matplotlib.axes.Axes) -- Axes for the plot.
     """
     y_err = (groups_df.iloc[:, -1] - groups_df.iloc[:, -2]) / 2
-    ax.bar(x=groups_df.index, height=groups_df['mean'], width=0.5)
-    ax.errorbar(x=groups_df.index, y=groups_df['mean'], yerr=y_err, color='k', fmt='none')
+    ax.errorbar(
+        x=groups_df.index,
+        y=groups_df['mean'],
+        yerr=y_err,
+        fmt='o', linewidth=1.0, capsize=4.0)
+    ax.set_xticks(groups_df.index)
+    ticks = ax.get_xticks()
+    delta = np.mean(np.diff(ticks))
+    bounds = ticks[[0, -1]]
+    ax.set_xlim(bounds + np.array([-delta/2, delta/2]))
+    ax.grid(alpha=0.5)
